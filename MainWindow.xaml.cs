29fd0f5c6a5f83d8c982994c9985c54968c7c65f
PashaahsaP <ppsairda@yandex.ru>
2021-11-29 08:59:25 +0700

    add dialog and drag and drop

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+ 
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace weight_2
 {
@@ -22,7 +24,35 @@ namespace weight_2
     {
         public MainWindow()
         {
+           
             InitializeComponent();
+            
+            
+        }
+
+        private void GroupBox_DragEnter(object sender, DragEventArgs e)//todo in mwwm
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Copy;
+        }
+
+        private void GroupBox_Drop(object sender, DragEventArgs e)
+        {
+            foreach (var item in (string[])e.Data.GetData(DataFormats.FileDrop))//todo in mwwm
+            {
+                
+            } 
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)//todo in mwwm
+        {
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            dialog.Title = "Выбери текстовый файл";
+            dialog.InitialDirectory = @"C:\Users\psair\Desktop";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+            }
         }
     }
 }
